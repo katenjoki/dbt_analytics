17ba5eaff9ebe1dd53f84bb433301ae04f83ce77
katenjoki98@gmail.com <katenjoki98@gmail.com>
2023-07-12 18:45:33 +0000

    update on stg_2017 and stg_2018

--- models/staging/stg_2018_data.sql
+++ models/staging/stg_2018_data.sql
@@ -16,9 +16,3 @@ select
 
 from {{ source("staging", "production_table_2018") }}
 
--- dbt build --m <model.sql> --var 'is_test_run: false'
-{% if var('is_test_run', default=true) %}
-
-    limit 100
-
-{% endif%}
