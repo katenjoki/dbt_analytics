cd0c8fb04199bae3ce4809182770527fe6697c3e
katenjoki98@gmail.com <katenjoki98@gmail.com>
2023-07-15 13:27:56 +0000

    update on coordinates

--- models/core/county_codes.sql
+++ models/core/county_codes.sql
@@ -1,3 +1,3 @@
 {{ config(materialized="table") }}
 
-select county_code,county,latitude,longitude from {{ ref('county_code_lookup')}}
+select county_code,county,coordinates from {{ ref('county_code_lookup')}}
